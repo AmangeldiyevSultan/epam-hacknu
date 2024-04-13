ba49d5a18dc054189bd996e427043e920dea3dce
s.amangeldiyev <sultanamangeldiyev@Sultans-MacBook-Pro.local>
2024-04-13 13:08:14 +0500

    ssl

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter_epam_kazakh/core/utils/logger.dart';
 import 'package:flutter_epam_kazakh/src/app/logic/app_runner.dart';
 
-// Sultan
+// Sultan S
 void main() {
   logger.runLogging(() {
     runZonedGuarded(
